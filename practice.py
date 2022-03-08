53347133d5da57d1be48045bf83a50aed3d7a697
slo-ow <icet25@naver.com>
2022-03-08 18:37:55 +0900

    python 탈출문자

--- practice.py
+++ practice.py
@@ -1,19 +1,20 @@
-# 방법1
-print("나는 %d살 입니다." % 20)
-print("나는 %s을 좋아해요." % "파이썬")
-print("Apple 은 %c로 시작해요." % "A")
-print("나는 %s색과 %s색을 좋아해요." % ("파랑", "빨강"))
+# \n : 줄바꿈
+print("백문이 불여일견\n백견이 불여일타")
 
-# 방법2
-print("나는 {}살 입니다.".format(20))
-print("나는 {}색과 {}색을 좋아해요.".format("파랑", "빨강"))
-print("나는 {0}색과 {1}색을 좋아해요.".format("파랑", "빨강"))
-print("나는 {1}색과 {0}색을 좋아해요.".format("파랑", "빨강"))
+# 저는 "나도코딩" 입니다.
+# print("저는 '나도코딩' 입니다.")
+# print('저는 "나도코딩" 입니다.')
+print("저는 \"나도코딩\" 입니다.")
+print("저는 \'나도코딩\' 입니다.")
 
-# 방법3
-print("나는 {age}살이며, {color}색을 좋아해요.".format(age=20, color="빨강"))
+# \\ : 문장 내에서 \
+print("Volumes\\Project\\python-basic")
 
-# 방법4 (v3.6 이상~)
-age = 21
-color = "초록"
-print(f"나는 {age}살이며, {color}색을 좋아해요.")
+# \r : 커서를 맨 앞으로 이동
+print("Red Apple\rPine")
+
+# \b : 백스페이스 (한 글자 삭제)
+print("Redd\bApple")
+
+# \t : 탭
+print("Red\tApple")
