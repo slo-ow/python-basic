7c624d3959f478550acc2bd90a3f4df929dd75ea
slo-ow <icet25@naver.com>
2022-03-08 18:30:38 +0900

    python 문자열 포맷

--- practice.py
+++ practice.py
@@ -1,15 +1,19 @@
-python = "Python is Amazing"
-print(python.lower())
-print(python.upper())
-print(python[0].isupper())
-print(len(python))
-print(python.replace("Python", "Java"))
+# 방법1
+print("나는 %d살 입니다." % 20)
+print("나는 %s을 좋아해요." % "파이썬")
+print("Apple 은 %c로 시작해요." % "A")
+print("나는 %s색과 %s색을 좋아해요." % ("파랑", "빨강"))
 
-index = python.index("n")
-print(index)
-index = python.index("n", index + 1)
-print(index)
-index = python.find("Java")
-print(index)
-index = python.index("Java")
-print(index)
+# 방법2
+print("나는 {}살 입니다.".format(20))
+print("나는 {}색과 {}색을 좋아해요.".format("파랑", "빨강"))
+print("나는 {0}색과 {1}색을 좋아해요.".format("파랑", "빨강"))
+print("나는 {1}색과 {0}색을 좋아해요.".format("파랑", "빨강"))
+
+# 방법3
+print("나는 {age}살이며, {color}색을 좋아해요.".format(age=20, color="빨강"))
+
+# 방법4 (v3.6 이상~)
+age = 21
+color = "초록"
+print(f"나는 {age}살이며, {color}색을 좋아해요.")
