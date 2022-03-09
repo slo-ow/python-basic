f924b4f8b33b0c1cab0cea1a635d385654be9572
slo-ow <icet25@naver.com>
2022-03-09 20:56:30 +0900

    while 연습

---
+++ while.py
@@ -0,0 +1,14 @@
+# while
+# customer = "토르"
+# index = 5
+# while index >= 1:
+#     print("{0}, 커피가 준비 되었습니다. {1} 번 남았어요.".format(customer, index))
+#     index -= 1
+#     if index == 0:
+#         print("커피는 폐기처분되었습니다.")
+
+customer = "토르"
+person = "unknown"
+while person != customer:
+    print("{0}, 커피가 준비 되었습니다.".format(customer))
+    person = input("이름이 어떻게 되세요?")
