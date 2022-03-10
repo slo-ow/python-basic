ae2a868c9eb71d1cc98a201cb7b5a2b76f849c17
slo-ow <icet25@naver.com>
2022-03-10 21:01:04 +0900

    함수 가변인자

--- function.py
+++ function.py
@@ -44,10 +44,29 @@
 # profile("유재석")
 # profile("김태호")
 
-def profile(name, age, main_lang):
-    print(name, age, main_lang)
+# def profile(name, age, main_lang):
+#     print(name, age, main_lang)
 
 
-# 인자값의 순서가 바뀌어도 출력값의 순서에는 영향을 받지 않는다.
-profile(name="유재석", main_lang="파이썬", age=20)
-profile(main_lang="자바", age=25, name="김태호")
+# # 인자값의 순서가 바뀌어도 출력값의 순서에는 영향을 받지 않는다.
+# profile(name="유재석", main_lang="파이썬", age=20)
+# profile(main_lang="자바", age=25, name="김태호")
+
+# def profile(name, age, lang1, lang2, lang3, lang4, lang5):
+#     print("이름: {0}\t나이 : {1}\t".format(
+#         name, age), end=" ")  # end=" "줄바꿈을 하지 않음
+#     print(lang1, lang2, lang3, lang4, lang5)
+
+
+# profile("유재석", 20, "Python", "Java", "C", "C++", "C#")
+# profile("김태호", 25, "Kotlin", "Swift", "", "", "")
+
+def profile(name, age, *language):  # *language 가변인자 사용 (서로 다른 인자값을 받아야할 때에 사용함)
+    print("이름: {0}\t나이 : {1}\t".format(name, age), end=" ")
+    for lang in language:
+        print(lang, end=" ")
+    print()
+
+
+profile("유재석", 20, "Python", "Java", "C", "C++", "C#", "JavaScript")
+profile("김태호", 25, "Kotlin", "Swift")
