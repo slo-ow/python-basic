cc11afb14afa88c4e52cfe9d8a44ea401556ee59
slo-ow <icet25@naver.com>
2022-03-11 19:55:04 +0900

    class 기초 연습

---
+++ my_class.py
@@ -0,0 +1,80 @@
+# # 마린 : 공격 유닛, 군인. 총을 쏠 수 있음
+# name = "마린"  # 유닛의 이름
+# hp = 40  # 유닛의 체력
+# damage = 5  # 유닛의 공격력
+
+# print("{} 유닛이 생성되었습니다.".format(name))
+# print("체력 {0}, 공격력 {1}\n".format(hp, damage))
+
+# # 탱크 : 공격 유닛, 탱크. 포를 쏠 수 있는데, 일반 모드 / 시즈 모드.
+# tank_name = "탱크"
+# tank_hp = 150
+# tank_damage = 35
+
+
+# print("{} 유닛이 생성되었습니다.".format(tank_name))
+# print("체력 {0}, 공격력 {1}\n".format(tank_hp, tank_damage))
+
+
+# class Unit:
+#     # __init__ = 생성자
+#     def __init__(self, name, hp, damage):
+#         # self.변수 (멤버변수)
+#         self.name = name
+#         self.hp = hp
+#         self.damage = damage
+#         print("{0} 유닛이 생성 되었습니다.".format(self.name))
+#         print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
+
+
+# marine1 = Unit("마린", 40, 5)
+# marine2 = Unit("마린", 40, 5)
+# tank = Unit("탱크", 150, 35)
+
+# wraith1 = Unit("레이스", 80, 5)
+# print("유닛 이름 : {0}, 공격력 : {1}".format(wraith1.name, wraith1.damage))
+
+# wraith2 = Unit("빼앗은 레이스", 80, 5)
+# wraith2.clocking = True  # clocking 이라는 변수에 True 값을 넣어줌
+
+# if wraith2.clocking == True:
+#     print("{0} 는 현재 클로킹 상태입니다.".format(wraith2.name))
+
+class Unit:
+    # __init__ = 생성자
+    def __init__(self, name, hp, damage):
+        # self.변수 (멤버변수)
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+        print("{0} 유닛이 생성 되었습니다.".format(self.name))
+        print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
+
+# 공격 유닛
+
+
+class AttackUnit:
+    def __init__(self, name, hp, damage):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+
+    def attack(self, location):
+        print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]"
+              .format(self.name, location, self.damage))
+
+    def damaged(self, damage):
+        print("{0} : {1}  데미지를 입었습니다.".format(self.name, self.damage))
+        self.hp -= damage
+        print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{0} : 파괴되었습니다.".format(self.name))
+
+
+# 공격 유닛
+firebat1 = AttackUnit("파이어뱃", 50, 16)
+firebat1.attack("5시")
+
+# 공격 2번 받는다고 가정
+firebat1.damaged(25)
+firebat1.damaged(25)
